2e4697d43935da4ace1fdb599d75ac1a7e6ade69
Kedar Khaire <kedark@google.com>
2024-12-26 17:27:29 +0530

    Update UiTest.php

--- modules/apigee_edge_teams/tests/src/Functional/UiTest.php
+++ modules/apigee_edge_teams/tests/src/Functional/UiTest.php
@@ -167,7 +167,7 @@ class UiTest extends ApigeeEdgeTeamsFunctionalTestBase {
     // Create a new team and check whether the link to the team is visible on
     // the listing page.
     $this->clickLink('Add team');
-    $team_name = $team_display_name = mb_strtolower(substr(str_shuffle('abcdefghjkmnpqrstuvwxyz'),0,8));
+    $team_name = $team_display_name = substr(str_shuffle('abcdefghjkmnpqrstuvwxyz'),0,8);
     $this->submitForm([
       'name' => $team_name,
       'displayName[0][value]' => $team_display_name,
@@ -187,7 +187,7 @@ class UiTest extends ApigeeEdgeTeamsFunctionalTestBase {
     // Update the team and check whether the updated name and field values are
     // visible on the listing and canonical pages.
     $this->clickLink('Edit');
-    $team_modified_display_name = $this->randomMachineName();
+    $team_modified_display_name = substr(str_shuffle('abcdefghjkmnpqrstuvwxyz'),0,8);
     $this->submitForm([
       'displayName[0][value]' => $team_modified_display_name,
       'field_integer[0][value]' => $this->fields['integer']['data_edited'],
@@ -241,7 +241,7 @@ class UiTest extends ApigeeEdgeTeamsFunctionalTestBase {
 
     // Update the previously created team app and check the updated values.
     $this->clickLink('Edit');
-    $team_app_1_modified_display_name = $this->randomMachineName();
+    $team_app_1_modified_display_name = substr(str_shuffle('abcdefghjkmnpqrstuvwxyz'),0,8);
     $this->submitForm([
       'displayName[0][value]' => $team_app_1_modified_display_name,
       'field_integer[0][value]' => $this->fields['integer']['data_edited'],
