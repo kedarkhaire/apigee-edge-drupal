1a9b72414e8fa92aa7b1599158aec46814128c8f
Kedar Khaire <kedark@google.com>
2024-12-26 20:11:53 +0530

    Update UiTest.php

--- modules/apigee_edge_teams/tests/src/Functional/UiTest.php
+++ modules/apigee_edge_teams/tests/src/Functional/UiTest.php
@@ -269,7 +269,7 @@ class UiTest extends ApigeeEdgeTeamsFunctionalTestBase {
     $this->drupalGet($this->team->toUrl('collection'));
     $this->assertSession()->linkExists($team_modified_display_name);
     $this->clickLink('Team Apps');
-    $this->assertSession()->pageTextContains('There are no team apps yet.');
+    $this->assertSession()->statusCodeEquals(200);
     $this->assertSession()->pageTextContains($team_app_1_modified_display_name);
     $this->assertSession()->linkExists($team_app_1_modified_display_name);
 
