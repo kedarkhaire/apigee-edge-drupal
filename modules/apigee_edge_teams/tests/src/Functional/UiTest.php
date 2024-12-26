3f1af8340c6d0ef822863e74f7c89a5ccfecc889
Kedar Khaire <kedark@google.com>
2024-12-26 15:43:44 +0530

    Update UiTest.php

--- modules/apigee_edge_teams/tests/src/Functional/UiTest.php
+++ modules/apigee_edge_teams/tests/src/Functional/UiTest.php
@@ -257,9 +257,9 @@ class UiTest extends ApigeeEdgeTeamsFunctionalTestBase {
     // Login with the default user, the created team app should be visible on
     // the team app administer collection page (/team-apps).
     $this->drupalLogin($this->account);
-    echo "Team" . "\r\n";
+    echo "Team \r\n";
     print_r($this->team);
-    echo "\r\n" . "Collection" . "\r\n"";
+    echo "\r\n Collection \r\n";
     print_r($this->team->toUrl('collection'));
     $this->drupalGet($this->team->toUrl('collection'));
     $this->assertSession()->linkExists($team_modified_display_name);
